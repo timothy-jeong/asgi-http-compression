28a3abd70532af777d2dafccaa287a321d3c5fda
minwoo <minwoo9621@gmail.com>
2025-11-23 19:49:02 +0900

    Add ASGI type definitions in types.py for improved type hinting

---
+++ src/asgi_http_compression/types.py
@@ -0,0 +1,10 @@
+from typing import Any, Awaitable, Callable
+
+
+
+# ASGI types
+Scope = dict[str, Any]
+Message = dict[str, Any]
+Receive = Callable[[], Awaitable[Message]]
+Send = Callable[[Message], Awaitable[None]]
+ASGIApp = Callable[[Scope, Receive, Send], Awaitable[None]]
